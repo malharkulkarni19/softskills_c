22641155737658c132045536050b06fafd1adf56
Malhar <malharkulkarni19@gmail.com>
2022-10-22 23:50:53 +0530

    C

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>  //it is a library, also have 20-30 libraries
+#define PI = 3.14  //any define in the program
+//all the upper zone is called as pre-processer-
+
+int main()   //it is main function in which we return the program
+{
+	printf("Hello, World!\n");   //what will be in output
+	
+	return 0;
+}
